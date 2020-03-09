bde5d48682ee994ae5265b92d8770f5e2a87517c
Ghiath2017 <g.alshamaa@hotmail.com>
2020-03-09 17:00:56 +0100

    CORS HEADERS MIDDLEWARE

--- api/app.js
+++ api/app.js
@@ -10,3 +10,16 @@ const { Task } = require('./database/models');
 
 // Load middleware
 app.use(bodyParser.json());
+
+// CORS HEADERS MIDDLEWARE
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    // allow preflight
+    if (req.method === 'OPTIONS') {
+        res.send(200);
+    } else {
+        next();
+    }
+});

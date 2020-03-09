6e4c4a511e99b4b9f64e52e6611e67ddcaae1555
Ghiath2017 <g.alshamaa@hotmail.com>
2020-03-09 16:40:44 +0100

    added a task model

---
+++ api/database/models/task.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+
+const TaskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    status: {
+        type: Boolean,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+})
+
+const Task = mongoose.model('Task', TaskSchema);
+
+module.exports = { Task }

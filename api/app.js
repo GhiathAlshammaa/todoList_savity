6caee66c869219d5c00d5afe77246da14f7721f5
Ghiath2017 <g.alshamaa@hotmail.com>
2020-03-09 16:59:47 +0100

    import all important dependencies and the model

---
+++ api/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+const { mongoose } = require('./database/mongoose');
+
+const bodyParser = require('body-parser');
+
+// Load in the mongoose models
+const { Task } = require('./database/models');
